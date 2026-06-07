96f41c8c2330de66dbbd8e987f9cfaea48863e69
pithlessly <79002325+pithlessly@users.noreply.github.com>
2026-06-07 14:48:17 -0700

    create type aliases for a little better type-safety

--- src/reduction.rs
+++ src/reduction.rs
@@ -5,21 +5,26 @@
 // - modeling
 // - roots
 
-use z3::ast::{BV, Datatype};
-
 use crate::{ir::Ir, parse_input::Latency, sorts::SolverSorts};
+
+mod dt {
+    use z3::ast::{BV, Datatype};
+    pub type IrNodeId = Datatype; // of sort `sorts.ir_node_id`
+    pub type MachineNodeId = BV; // of sort `sorts.machine_node_id`
+    pub type MachineNode = Datatype; // of sort `sorts.machine_node.sort`
+}
 
 #[derive(Debug)]
 pub struct OutputSlot {
-    pub instr: Datatype, // M (t_machine_node)
-    pub model: Datatype, // m (t_ir_node_id)
+    pub instr: dt::MachineNode, // M[i]
+    pub model: dt::IrNodeId,    // m[i]
 }
 
 impl OutputSlot {
     pub fn new(sorts: &SolverSorts, idx: u32) -> Self {
         Self {
-            instr: Datatype::new_const(format!("M_{idx}"), &sorts.machine_node),
-            model: Datatype::new_const(format!("m_{idx}"), &sorts.ir_node_id),
+            instr: dt::MachineNode::new_const(format!("M_{idx}"), &sorts.machine_node.sort),
+            model: dt::IrNodeId::new_const(format!("m_{idx}"), &sorts.ir_node_id.sort),
         }
     }
 }
@@ -27,7 +32,7 @@ impl OutputSlot {
 #[derive(Debug)]
 pub struct Variables {
     pub output_program: Box<[OutputSlot]>,
-    pub root_witnesses: Box<[BV]>,
+    pub root_witnesses: Box<[dt::MachineNodeId]>, // ρ[i]
 }
 
 impl Variables {
@@ -36,7 +41,7 @@ impl Variables {
             .map(|i| OutputSlot::new(&sorts, i))
             .collect();
         let root_witnesses = (0..ir.program.roots.len())
-            .map(|i| BV::new_const(format!("ρ_{i}"), sorts.machine_node_id_bitcount))
+            .map(|i| dt::MachineNodeId::new_const(format!("ρ_{i}"), sorts.machine_node_id_bitcount))
             .collect();
 
         Self {
